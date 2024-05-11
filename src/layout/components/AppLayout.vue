c38d83206151d2dd76f676b0d0904a48ab2a860e
Caleb <agyakojo4321@gmail.com>
2024-05-11 22:34:08 +0000

    feat: :fire: shadcn components and tailwind css setup done

--- src/layout/components/AppLayout.vue
+++ src/layout/components/AppLayout.vue
@@ -1,11 +1,11 @@
 <template>
-  <main class="container">
-    <div class="sidebar">
+  <main class="w-screen h-screen border-4">
+    <div class="">
       <RouterLink v-for="route in appRoutes" :key="route.path" :to="route.path">
         {{ route.meta.label }}
       </RouterLink>
     </div>
-    <div class="wrapper"><RouterView /></div>
+    <div class=""><RouterView /></div>
   </main>
 </template>
 
@@ -17,21 +17,4 @@ import { RouterView, RouterLink } from 'vue-router'
 const appRoutes = computed(() => routes.filter((d) => d.meta.layout === 'App'))
 </script>
 
-<style scoped>
-.container {
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: space-between;
-}
-.sidebar {
-  width: 200px;
-  border: 1px solid gray;
-  display: flex;
-  flex-direction: column;
-}
-.wrapper {
-  flex: 1;
-  padding: 5px;
-}
-</style>
+<style scoped></style>
